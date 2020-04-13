223a8e028a4503f0b9fae71245774b766fc015a7
liranmeir <liranmeir@gmail.com>
2020-04-13 17:21:32 +0300

    adding border the image

--- src/components/photo/Photo.jsx
+++ src/components/photo/Photo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./photo.scss";
 export const Photo = ({ key, url }) => {
   return (
-    <div key={key}>
+    <div key={key} className='photo'>
       <img src={url} />
     </div>
   );
